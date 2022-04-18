23185e8cb032a70f08cc0c9f605bf2131be00761
Kisang28 <85912291+Kisang28@users.noreply.github.com>
2022-04-18 23:19:49 +0800

    put mutators and accessors

--- src/com/ecpe308/sample/Pet.java
+++ src/com/ecpe308/sample/Pet.java
@@ -3,4 +3,28 @@ package com.ecpe308.sample;
 public class Pet {
     public String name, breed; //persian, labrador, golden retriever
     public String pet_type;  //cat/dog
+
+    public String getPetName() {
+        return name;
+    }
+
+    public void setPetName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getPet_Type() {
+        return pet_type;
+    }
+
+    public void setPet_Type(String pet_type) {
+        this.pet_type = pet_type;
+    }
 }
