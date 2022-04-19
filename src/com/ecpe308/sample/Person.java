c0cc90ad2af534176a95a1d0798a94c9697c19b1
michaelangelosalvio <m.salvio@usls.edu.ph>
2022-04-19 11:15:54 +0800

    make attributes of Person private

--- src/com/ecpe308/sample/Person.java
+++ src/com/ecpe308/sample/Person.java
@@ -1,6 +1,38 @@
 package com.ecpe308.sample;
 
 public class Person {
-    public String lastName, firstName, address;
-    public int age;
+    private String lastName, firstName, address;
+    private int age;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
