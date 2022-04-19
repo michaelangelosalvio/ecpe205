5ef1d67446f7f4b7710ed47d7b5490f4b5d3c50d
John Raphael <Johnrserantes424@gmail.com>
2022-04-19 12:30:37 +0800

    Accessors and Mutators added

--- src/com/ecpe308/sample/Pet.java
+++ src/com/ecpe308/sample/Pet.java
@@ -3,4 +3,30 @@ package com.ecpe308.sample;
 public class Pet {
     public String name, breed; //persian, labrador, golden retriever
     public String pet_type;  //cat/dog
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getType() {
+        return pet_type;
+    }
+
+    public void setType(String pet_type) {
+        this.pet_type = pet_type;
+    }
+
 }
+
