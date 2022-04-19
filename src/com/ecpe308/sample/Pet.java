2d762074943db8b4406be5c94424d33b38ab2b49
gwynneth.g <s2020018@usls.edu.ph>
2022-04-19 13:22:43 +0800

    with changes

--- src/com/ecpe308/sample/Pet.java
+++ src/com/ecpe308/sample/Pet.java
@@ -4,3 +4,23 @@ public class Pet {
     public String name, breed; //persian, labrador, golden retriever
     public String pet_type;  //cat/dog
 }
+public String getname(){
+        return name;
+    }
+    public String getbreed(){
+        return breed;
+    }
+    public String getpet_type(){
+        return pet_type;
+    }
+    public void setname(String newname){
+        this.name = newname;
+    }
+    public void setbreed(String newbreed){
+        this.breed = newbreed;
+    }  
+    public void setpet_type(String newpet_type){
+        this.pet_type = newpet_type;  
+    }   
+     
+}
