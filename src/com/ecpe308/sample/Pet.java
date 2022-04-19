be0e5f28e161701f5a3df7376f008229740e5a02
Chaaang <johnmarchangggggg@gmail.com>
2022-04-19 12:01:11 +0800

    add accessors and mutators for pet file

--- src/com/ecpe308/sample/Pet.java
+++ src/com/ecpe308/sample/Pet.java
@@ -3,4 +3,31 @@ package com.ecpe308.sample;
 public class Pet {
     public String name, breed; //persian, labrador, golden retriever
     public String pet_type;  //cat/dog
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getpet_type() {
+        return pet_type;
+    }
+
+    public void setpet_type(String pet_type) {
+        this.pet_type = pet_type;
+    }
+
+    
 }
