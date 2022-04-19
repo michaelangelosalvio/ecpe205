a23a5636ee67b211547ed9ac6a44ee9e0599344c
michaelangelosalvio <m.salvio@usls.edu.ph>
2022-04-19 11:07:24 +0800

    set attributes to pet and person

--- src/com/ecpe308/sample/Person.java
+++ src/com/ecpe308/sample/Person.java
@@ -1,4 +1,6 @@
 package com.ecpe308.sample;
 
 public class Person {
+    public String lastName, firstName, address;
+    public int age;
 }
--- src/com/ecpe308/sample/Pet.java
+++ src/com/ecpe308/sample/Pet.java
@@ -1,4 +1,6 @@
 package com.ecpe308.sample;
 
 public class Pet {
+    public String name, breed; //persian, labrador, golden retriever
+    public String pet_type;  //cat/dog
 }
